8a38baa3919348a4a87e62ff132b25cb5284e18c
TeeSeal <alex.theoddor@gmail.com>
2017-12-24 19:22:10 +0200

    Write base for User

--- src/User.js
+++ src/User.js
@@ -1,3 +1,44 @@
-class User {}
+const { Oauth } = require('oauth')
+const baseURL = 'https://api.twitter.com/1.1/'
+
+class User extends Oauth {
+  constructor(appKey, appSecret, opts) {
+    super(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      appKey,
+      appSecret,
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    )
+
+    this.appKey = appKey
+    this.appSecret = appSecret
+    this.userToken = opts.userToken || null
+    this.userSecret = opts.userSecret || null
+  }
+
+  get(endpoint, opts) {
+    return new Promise((resolve, reject) => {
+      const { userToken, userSecret } =
+        opts.userToken && opts.userSecret ? opts : this
+
+      let url = `${baseURL}${endpoint}.json`
+      if (opts.params) {
+        const params = Object.entries(opts.params).map(pair => {
+          return pair.map(text => encodeURIComponent(text)).join('=')
+        })
+
+        url += `?${params}`
+      }
+
+      super.get(url, userToken, userSecret, (err, data) => {
+        if (err) return reject(err)
+        resolve(data)
+      })
+    })
+  }
+}
 
 module.exports = User
