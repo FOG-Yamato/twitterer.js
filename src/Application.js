4b71eef621077c9aa8746d0ad257a6d34d232a0b
TeeSeal <alex.theoddor@gmail.com>
2017-12-25 21:21:55 +0200

    Add HTTP methods for Application

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,8 @@
 const fetch = require('node-fetch')
+const { URL } = require('url')
+
+const TWITTER_API = 'https://api.twitter.com/1.1/'
+const AUTH_ENDPOINT = 'https://api.twitter.com/oauth2/token'
 
 class Application {
   constructor(key, secret) {
@@ -7,21 +11,63 @@ class Application {
     this.token = null
   }
 
+  get(endpoint, opts) {
+    return this.request(endpoint, opts)
+  }
+
+  post(endpoint, opts) {
+    return this.request(endpoint, { ...opts, method: 'POST' })
+  }
+
+  delete(endpoint, opts) {
+    return this.request(endpoint, { ...opts, method: 'DELETE' })
+  }
+
+  async request(endpoint, opts) {
+    if (endpoint !== AUTH_ENDPOINT) {
+      if (!this.token) {
+        try {
+          await this.auth()
+        } catch (err) {
+          return Promise.reject(err)
+        }
+      }
+
+      opts.headers = { ...opts.headers, Authorization: `Bearer ${this.token}` }
+    }
+
+    const url = this.buildURL(endpoint, opts.params, true)
+    return fetch(url, opts).then(res => res.json())
+  }
+
   async auth() {
     const encoded = Buffer.from(`${this.key}:${this.secret}`).toString('base64')
 
-    const data = await fetch('https://api.twitter.com/oauth2/token', {
-      method: 'POST',
+    const data = await this.post(AUTH_ENDPOINT, {
       body: 'grant_type=client_credentials',
       headers: {
         Authorization: `Basic ${encoded}`,
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8.'
       }
-    }).then(res => res.json())
+    })
 
-    if (data.errors) return Promise.reject(data.errors[0])
+    if (data.errors) return Promise.reject(new Error(data.errors[0].message))
     this.token = data.access_token
     return Promise.resolve(this)
+  }
+
+  buildURL(url, params, dotJSON) {
+    if (dotJSON && url !== AUTH_ENDPOINT) url += '.json'
+
+    url = new URL(url, TWITTER_API)
+
+    if (params) {
+      for (const [key, value] of Object.entries(params)) {
+        url.searchParams.append(key, value)
+      }
+    }
+
+    return url.toString()
   }
 }
 
