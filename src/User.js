8a9a4c3e6ca537ddb49880c7cbc14fe5857505d5
TeeSeal <alex.theoddor@gmail.com>
2017-12-26 19:57:48 +0200

    User scope methods

--- src/User.js
+++ src/User.js
@@ -1,44 +1,88 @@
 const { OAuth } = require('oauth')
-const baseURL = 'https://api.twitter.com/1.1/'
+const { URL } = require('url')
+const TWITTER_API = 'https://api.twitter.com/1.1/'
 
-class User extends OAuth {
-  constructor(appKey, appSecret, opts) {
-    super(
+class User {
+  constructor(appKey, appSecret, opts = {}) {
+    this.session = new OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
       appKey,
       appSecret,
-      '1.0A',
+      '1.0',
+      opts.callbackUrl || null,
+      'HMAC-SHA1',
       null,
-      'HMAC-SHA1'
+      {
+        Accept: '*/*',
+        Connection: 'close',
+        'User-Agent': 'twitter.js'
+      }
     )
-
-    this.appKey = appKey
-    this.appSecret = appSecret
     this.userToken = opts.userToken || null
     this.userSecret = opts.userSecret || null
   }
 
-  get(endpoint, opts) {
+  get(endpoint, opts = {}) {
     return new Promise((resolve, reject) => {
-      const { userToken, userSecret } =
-        opts && opts.userToken && opts.userSecret ? opts : this
-
-      let url = `${baseURL}${endpoint}.json`
-
-      if (opts && opts.params) {
-        const params = Object.entries(opts.params).map(pair => {
-          return pair.map(text => encodeURIComponent(text)).join('=')
-        })
-
-        url += `?${params}`
-      }
-
-      super.get(url, userToken, userSecret, (err, data) => {
-        if (err) return reject(err)
-        resolve(data)
-      })
+      const url = this.buildURL(endpoint, opts.params, true)
+      this.session.get(
+        url,
+        this.userToken,
+        this.userSecret,
+        (err, body, res) => {
+          if (!err && res.statusCode == 200) {
+            let limits = {
+              'x-rate-limit-limit': res.headers['x-rate-limit-limit'],
+              'x-rate-limit-remaining': res.headers['x-rate-limit-remaining'],
+              'x-rate-limit-reset': res.headers['x-rate-limit-reset']
+            }
+            resolve({ body, limits })
+          } else {
+            reject({ err, res, body })
+          }
+        }
+      )
     })
+  }
+
+  post(endpoint, opts = {}) {
+    return new Promise((resolve, reject) => {
+      const url = this.buildURL(endpoint, opts.params, true)
+      this.session.post(
+        url,
+        this.userToken,
+        this.userSecret,
+        opts.body,
+        'application/x-www-form-urlencoded',
+        (err, body, res) => {
+          if (!err && res.statusCode == 200) {
+            let limits = {
+              'x-rate-limit-limit': res.headers['x-rate-limit-limit'],
+              'x-rate-limit-remaining': res.headers['x-rate-limit-remaining'],
+              'x-rate-limit-reset': res.headers['x-rate-limit-reset']
+            }
+            resolve({ body, limits })
+          } else {
+            reject({ err, res, body })
+          }
+        }
+      )
+    })
+  }
+
+  buildURL(url, params, dotJSON) {
+    if (dotJSON) url += '.json'
+
+    url = new URL(url, TWITTER_API)
+
+    if (params) {
+      for (const [key, value] of Object.entries(params)) {
+        url.searchParams.append(key, value)
+      }
+    }
+
+    return url.toString()
   }
 }
 
